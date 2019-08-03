5b0ec4e9e674075287311b35e409051358878a13
Subrata Halder <43924070+SubrataHalder62@users.noreply.github.com>
2019-08-03 15:26:45 +0530

    Add files via upload

---
+++ Clothing.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  2 16:02:32 2019
+
+@author: USER
+"""
+
+#Dataset
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+#Building the Model
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+#Compiling the Model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+print('Model Built !!')
+#Train the Model
+model.fit(train_images, train_labels, epochs=5)
+print('Model Trained !!')
+
+#Test the Model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print('Test accuracy:', test_acc)
+
+#Make Predictions
+predictions = model.predict(test_images)
